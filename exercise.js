0e0b365b9b289b184846e86cfd55d900c6cfa667
Kerollos Magdy <kerolloz@yahoo.com>
2019-10-20 16:31:03 +0200

    lint js, readme

--- exercise.js
+++ exercise.js
@@ -1,53 +1,49 @@
 // jshint esversion: 6
-const fs = require('fs');
+import { readdirSync, readFile, writeFile } from "fs";
 
 function writeExercises() {
-    let exercises = fs.readdirSync('exercises/');
-    console.log(exercises);
-    for (const exercise of exercises) {
-        document.write(`<li id="${exercise}" onclick="loadExercise(this.id)"><a href="#"> ${exercise} </a> </li>`);
-    }
+  let exercises = readdirSync("exercises/");
+  console.log(exercises);
+  for (const exercise of exercises) {
+    document.write(
+      `<li id="${exercise}" onclick="loadExercise(this.id)"><a href="#"> ${exercise} </a> </li>`
+    );
+  }
 }
 
-var MarkdownIt = require('markdown-it'),
-    md = new MarkdownIt();
+import MarkdownIt from "markdown-it";
+var md = new MarkdownIt();
 
 let currentExercise = null;
 
 function loadExercise(clicked_id) {
-    currentExercise = clicked_id;
-    fs.readFile('exercises/' + clicked_id + "/README.md", "utf8", (err, data) => {
-        if (err)
-            console.log("Something went wrong");
-        else
-            document.getElementById("exerciseDetails").innerHTML = md.render(data.replace(/<br>/g, "\n"));
-    });
-    fs.readFile('exercises/' + clicked_id + "/solution.cpp", "utf8", (err, data) => {
-        if (err)
-            console.log("Something went wrong");
-        else
-            myCodeMirror.setValue(data);
-    });
-    // show compile or check button
-
+  currentExercise = clicked_id;
+  readFile("exercises/" + clicked_id + "/README.md", "utf8", (err, data) => {
+    if (err) console.log("Something went wrong");
+    else
+      document.getElementById("exerciseDetails").innerHTML = md.render(
+        data.replace(/<br>/g, "\n")
+      );
+  });
+  readFile("exercises/" + clicked_id + "/solution.cpp", "utf8", (err, data) => {
+    if (err) console.log("Something went wrong");
+    else myCodeMirror.setValue(data);
+  });
+  // show compile or check button
 }
 function compile() {
-    const {
-        exec
-    } = require('child_process');
-    // save the written code in solution.cpp
-    const code = myCodeMirror.getValue();
-    fs.writeFile('exercises/' + currentExercise + "/solution.cpp", code, (err) => {
-        if (err) {
-            return console.log(err);
-        }
+  // save the written code in solution.cpp
+  const code = myCodeMirror.getValue();
+  writeFile("exercises/" + currentExercise + "/solution.cpp", code, err => {
+    if (err) {
+      return console.log(err);
+    }
 
-        console.log("The file was saved!");
-        // compile or check
-        // tell the user whether it's a correct or a wrong answer!
-        const exerciseNumber = currentExercise.split("-");
-        const addon = require('./build/Release/addon');
-        alert(addon.verify(parseInt(exerciseNumber[1])));
-    });
-
+    console.log("The file was saved!");
+    // compile or check
+    // tell the user whether it's a correct or a wrong answer!
+    const exerciseNumber = currentExercise.split("-");
+    const addon = require("./build/Release/addon");
+    alert(addon.verify(parseInt(exerciseNumber[1])));
+  });
 }
--- exercises/exercise-2/README.md
+++ exercises/exercise-2/README.md
@@ -1,10 +1,12 @@
 # Exercise 2
-## Create a class called time
-has separate int member data for hours, minutes, and seconds.(make thim private)<br>
-One constructor should initialize this data to 0 <br>
-Another constructor should initialize it to fixed values. <br>
-Another member function should display it, in 11:59:59 format. <br>
-Make three member function to return every member data name them ``getHours``, ``getMin`` & ``getSec`` <br>
 
-The final member function should add two objects of type time passed as arguments. <br>
-Make The appropriate member function const.<br>
+## Create a class called time
+
+has separate int member data for hours, minutes, and seconds.(make thim private)  
+One constructor should initialize this data to 0  
+Another constructor should initialize it to fixed values.  
+Another member function should display it, in 11:59:59 format.  
+Make three member function to return every member data name them `getHours`, `getMin` & `getSec`
+
+The final member function should add two objects of type time passed as arguments.  
+Make The appropriate member function const.
